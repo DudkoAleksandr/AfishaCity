f1694ff8ca69661acf7df4c43b89ec485d8cf042
Sanya9700 <sanya25051991@gmail.com>
2026-09-23 22:51:43 +0400

    change html and css

--- script.js
+++ script.js
@@ -1,48 +1,51 @@
 const eventsBlock = document.querySelector(".ivents__cards");
 
 const routes = [
-    {
-        id: 1,
-        date: '2026-09-22',
-        name: 'Рассвет',
-        distance: '24',
-        level: 'easy',
-        start: 'Ростовский акв',
-        text: 'Спокойный',
-    },
-    {
-        id: 1,
-        date: '2026-09-22',
-        name: 'Рассвет',
-        distance: '24',
-        level: 'easy',
-        start: 'Ростовский акв',
-        text: 'Спокойный',
-    },
-    {
-        id: 1,
-        date: '2026-09-22',
-        name: 'Рассвет',
-        distance: '24',
-        level: 'easy',
-        start: 'Ростовский акв',
-        text: 'Спокойный',
-    },
-    {
-        id: 1,
-        date: '2026-09-22',
-        name: 'Рассвет',
-        distance: '24',
-        level: 'easy',
-        start: 'Ростовский акв',
-        text: 'Спокойный',
-    }
-    
-]
+  {
+    id: 1,
+    date: "2026-09-22",
+    name: "Рассвет",
+    distance: "24",
+    level: "easy",
+    start: "Ростовский акв",
+    text: "Спокойный",
+    status: "Ближайший",
+  },
+  {
+    id: 1,
+    date: "2026-09-22",
+    name: "Рассвет1",
+    distance: "24 1",
+    level: "easy 1",
+    start: "Ростовский акв 1",
+    text: "Спокойный 1",
+    status: "Ближайший1",
+  },
+  {
+    id: 1,
+    date: "2026-09-22",
+    name: "Рассвет2",
+    distance: "24 2",
+    level: "easy 2",
+    start: "Ростовский акв 2",
+    text: "Спокойный 2",
+    status: "Ближайший2",
+  },
+  {
+    id: 1,
+    date: "2026-09-22",
+    name: "Рассвет3",
+    distance: "24 3",
+    level: "easy 3",
+    start: "Ростовский акв 3",
+    text: "Спокойный 3",
+    status: "Ближайший3",
+  },
+];
 
-function render(){
-    for( let route of routes){
-        const html = `
+function render() {
+  for (let route of routes) {
+    const html = `
                     <div class="ivent__card">
               <div class="ivent__dates">
                 <div class="ivent__date">
@@ -55,7 +58,7 @@ function render(){
                     src="./img/Badgeicon.png"
                     alt=""
                   />
-                  <p class="ivent__start-text">БЛИЖАЙШИЙ</p>
+                  <p class="ivent__start-text">${route.status}</p>
                 </div>
               </div>
               <p class="ivent__title">${route.name}</p>
@@ -68,14 +71,14 @@ function render(){
                   src="./img/RouteIcon.png"
                   alt=""
                 />
-                <p class="ivent__level-title">24 км</p>
+                <p class="ivent__level-title">${route.distance} км</p>
                 <div class="ivent__logos">
                   <img
                     class="ivent__logos-logo"
                     src="./img/EasyIcon.png"
                     alt=""
                   />
-                  <p class="ivent__logos-text">ЛЕГКО</p>
+                  <p class="ivent__logos-text">${route.level}</p>
                 </div>
               </div>
               <div class="ivent__adres">
@@ -84,12 +87,24 @@ function render(){
                   src="./img/RouteCardIcon.png"
                   alt=""
                 />
-                <p class="ivent__adress">Ростокинский акведук</p>
+                <p class="ivent__adress">${route.start}</p>
+              </div>
+                            <div class="card__change">
+                <div class="card__correct">
+                  <img
+                    class="card__correct-icon"
+                    src="./img/GhostIcon.png"
+                    alt=""
+                  />
+                  <a href=""><p class="card__correct-text">Редактировать</p></a>
+                </div>
+                <div class="card__del">
+                  <img class="card__del-img" src="./img/IconGlyph.png" alt="" />
+                </div>
               </div>
             </div>
         `;
-        eventsBlock.insertAdjacentHTML('beforeend', html)
-    }
-    
+    eventsBlock.insertAdjacentHTML("beforeend", html);
+  }
 }
-render()
+render();
